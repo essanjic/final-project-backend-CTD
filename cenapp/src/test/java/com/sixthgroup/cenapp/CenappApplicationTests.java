a151e5d64b660dbfc6d9a679177604b85424cece
Ael <essanjic@gmail.com>
2023-10-27 22:51:50 -0500

    feat: Adding project cenapp

---
+++ cenapp/src/test/java/com/sixthgroup/cenapp/CenappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixthgroup.cenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
